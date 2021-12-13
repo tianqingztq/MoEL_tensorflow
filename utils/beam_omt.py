77edee573eccc4e8362f3013ddbf4b3954e02048
Shaoyu Liu <sl4640@columbia.edu>
2021-12-13 12:25:49 -0500

    to tensorflow

--- utils/beam_omt.py
+++ utils/beam_omt.py
@@ -4,10 +4,11 @@
     https://github.com/OpenNMT/OpenNMT-py/blob/master/onmt/Beam.py
 """
 
-import torch
+#import torch
 import numpy as np
 from utils import config
-import torch.nn.functional as F
+#import torch.nn.functional as F
+import tensorflow as tf
 
 class Beam():
     ''' Beam search '''
@@ -18,14 +19,16 @@ class Beam():
         self._done = False
 
         # The score for each translation on the beam.
-        self.scores = torch.zeros((size,), dtype=torch.float, device=device)
+        #self.scores = torch.zeros((size,), dtype=torch.float, device=device)
+        self.scores = tf.zeros([size,], dtype = tf.float32)
         self.all_scores = []
 
         # The backpointers at each time-step.
         self.prev_ks = []
 
         # The outputs at each time-step.
-        self.next_ys = [torch.full((size,), config.PAD_idx, dtype=torch.long, device=device)]
+        #self.next_ys = [torch.full((size,), config.PAD_idx, dtype=torch.long, device=device)]
+        self.next_ys = [tf.fill([size,], config.PAD_idx, dtype=tf.int64)]
         self.next_ys[0][0] = config.SOS_idx
 
     def get_current_state(self):
@@ -42,19 +45,23 @@ class Beam():
 
     def advance(self, word_prob):
         "Update beam status and check if finished or not."
-        num_words = word_prob.size(1)
+        #num_words = word_prob.size(1)
+        num_words = word_prob.shape[1]
 
         # Sum the previous scores.
         if len(self.prev_ks) > 0:
-            beam_lk = word_prob + self.scores.unsqueeze(1).expand_as(word_prob)
+            #beam_lk = word_prob + self.scores.unsqueeze(1).expand_as(word_prob)
+            beam_lk = word_prob + tf.reshape(tf.expand_dims(self.scores, 1), word_prob.shape)
         else:
             beam_lk = word_prob[0]
 
-        flat_beam_lk = beam_lk.view(-1)
+        #flat_beam_lk = beam_lk.view(-1)
+        flat_beam_lk = tf.reshape(-1)
 
+        #best_scores, best_scores_id = flat_beam_lk.topk(self.size, 0, True, True) # 1st sort
+        #best_scores, best_scores_id = flat_beam_lk.topk(self.size, 0, True, True) # 2nd sort
         best_scores, best_scores_id = flat_beam_lk.topk(self.size, 0, True, True) # 1st sort
-        best_scores, best_scores_id = flat_beam_lk.topk(self.size, 0, True, True) # 2nd sort
-
+        
         self.all_scores.append(self.scores)
         self.scores = best_scores
 
@@ -73,7 +80,8 @@ class Beam():
 
     def sort_scores(self):
         "Sort the scores."
-        return torch.sort(self.scores, 0, True)
+        #return torch.sort(self.scores, 0, True)
+        return tf.sort(self.scores, 0, direction='DESCENDING')
 
     def get_the_best_score_and_idx(self):
         "Get the score of the best in the beam."
@@ -84,12 +92,14 @@ class Beam():
         "Get the decoded sequence for the current timestep."
 
         if len(self.next_ys) == 1:
-            dec_seq = self.next_ys[0].unsqueeze(1)
+            #dec_seq = self.next_ys[0].unsqueeze(1)
+            dec_seq = tf.expand_dims(self.next_ys[0],1)
         else:
             _, keys = self.sort_scores()
             hyps = [self.get_hypothesis(k) for k in keys]
             hyps = [[config.SOS_idx] + h for h in hyps]
-            dec_seq = torch.LongTensor(hyps)
+            #dec_seq = torch.LongTensor(hyps)
+            dec_seq = tf.Variable(hyps, dtype = tf.int64)
 
         return dec_seq
 
@@ -111,7 +121,7 @@ class Translator(object):
         self.lang = lang
         self.vocab_size = lang.n_words
         self.beam_size = config.beam_size
-        self.device = torch.device('cuda' if config.USE_CUDA else 'cpu')
+        #self.device = torch.device('cuda' if config.USE_CUDA else 'cpu')
 
 
     def beam_search(self, src_seq, max_dec_step):
@@ -124,14 +134,17 @@ class Translator(object):
         def collect_active_part(beamed_tensor, curr_active_inst_idx, n_prev_active_inst, n_bm):
             ''' Collect tensor parts associated to active instances. '''
 
-            _, *d_hs = beamed_tensor.size()
+            #_, *d_hs = beamed_tensor.size()
+            _, *d_hs = beamed_tensor.shape
             n_curr_active_inst = len(curr_active_inst_idx)
             new_shape = (n_curr_active_inst * n_bm, *d_hs)
 
-            beamed_tensor = beamed_tensor.view(n_prev_active_inst, -1)
+            #beamed_tensor = beamed_tensor.view(n_prev_active_inst, -1)
+            beamed_tensor = tf.reshape(beamed_tensor, -1)
             beamed_tensor = beamed_tensor.index_select(0, curr_active_inst_idx)
-            beamed_tensor = beamed_tensor.view(*new_shape)
-
+            #beamed_tensor = beamed_tensor.view(*new_shape)
+            beamed_tensor = tf.reshape(beamed_tensor, *new_shape)
+            
             return beamed_tensor
 
         def collate_active_info(src_seq, encoder_db, src_enc, inst_idx_to_position_map, active_inst_idx_list):
@@ -139,7 +152,8 @@ class Translator(object):
             # so the decoder will not run on completed sentences.
             n_prev_active_inst = len(inst_idx_to_position_map)
             active_inst_idx = [inst_idx_to_position_map[k] for k in active_inst_idx_list]
-            active_inst_idx = torch.LongTensor(active_inst_idx).to(self.device)
+            #active_inst_idx = torch.LongTensor(active_inst_idx).to(self.device)
+            active_inst_idx = tf.Variable(active_inst_idx, dtype = tf.int64)
 
             active_src_seq = collect_active_part(src_seq, active_inst_idx, n_prev_active_inst, n_bm)
             active_src_enc = collect_active_part(src_enc, active_inst_idx, n_prev_active_inst, n_bm)
@@ -155,19 +169,26 @@ class Translator(object):
 
             def prepare_beam_dec_seq(inst_dec_beams, len_dec_seq):
                 dec_partial_seq = [b.get_current_state() for b in inst_dec_beams if not b.done]
-                dec_partial_seq = torch.stack(dec_partial_seq).to(self.device)
-                dec_partial_seq = dec_partial_seq.view(-1, len_dec_seq)
+                #dec_partial_seq = torch.stack(dec_partial_seq).to(self.device)
+                dec_partial_seq = tf.stack(dec_partial_seq)
+                #dec_partial_seq = dec_partial_seq.view(-1, len_dec_seq)
+                dec_partial_seq = tf.reshape(dec_partial_seq, [-1, len_dec_seq])
                 return dec_partial_seq
 
             def prepare_beam_dec_pos(len_dec_seq, n_active_inst, n_bm):
-                dec_partial_pos = torch.arange(1, len_dec_seq + 1, dtype=torch.long, device=self.device)
-                dec_partial_pos = dec_partial_pos.unsqueeze(0).repeat(n_active_inst * n_bm, 1)
+                #dec_partial_pos = torch.arange(1, len_dec_seq + 1, dtype=torch.long, device=self.device)
+                dec_partial_pos = tf.range(1, len_dec_seq + 1, dtype = tf.int64)
+                #dec_partial_pos = dec_partial_pos.unsqueeze(0).repeat(n_active_inst * n_bm, 1)
+                dec_partial_pos = tf.tile(tf.expand_dims(dec_partial_pos, 0), [n_active_inst * n_bm, 1])
                 return dec_partial_pos
                   
             def predict_word(dec_seq, dec_pos, src_seq, enc_output, n_active_inst, n_bm, enc_batch_extend_vocab, extra_zeros, mask_src, encoder_db, mask_transformer_db, DB_ext_vocab_batch):
                 ## masking
-                mask_trg = dec_seq.data.eq(config.PAD_idx).unsqueeze(1)
-                mask_src = torch.cat([mask_src[0].unsqueeze(0)]*mask_trg.size(0),0)
+                #mask_trg = dec_seq.data.eq(config.PAD_idx).unsqueeze(1)
+                mask_trg = tf.expand_dims(tf.math.equal(dec_seq, config.PAD_idx), axis = 1)
+                
+                #mask_src = torch.cat([mask_src[0].unsqueeze(0)]*mask_trg.size(0),0)
+                mask_src = tf.concat([tf.expand_dims(mask_src[0], 0)]*mask_trg.shape[0],0)
                 dec_output, attn_dist = self.model.decoder(self.model.embedding(dec_seq), enc_output, (mask_src,mask_trg))
 
                 db_dist = None
@@ -175,7 +196,8 @@ class Translator(object):
                 prob = self.model.generator(dec_output,attn_dist,enc_batch_extend_vocab, extra_zeros, 1, True, attn_dist_db=db_dist)
                 #prob = F.log_softmax(prob,dim=-1) #fix the name later
                 word_prob = prob[:, -1]
-                word_prob = word_prob.view(n_active_inst, n_bm, -1)
+                #word_prob = word_prob.view(n_active_inst, n_bm, -1)
+                word_prob = tf.reshape(word_prob, [n_active_inst, n_bm, -1])
                 return word_prob
 
             def collect_active_inst_idx_list(inst_beams, word_prob, inst_idx_to_position_map):
@@ -207,42 +229,47 @@ class Translator(object):
                 all_hyp += [hyps]
             return all_hyp, all_scores
 
-        with torch.no_grad():
+        #with torch.no_grad():
             #-- Encode
-            enc_batch, _, _, enc_batch_extend_vocab, extra_zeros, _, _ = get_input_from_batch(src_seq)
+        enc_batch, _, _, enc_batch_extend_vocab, extra_zeros, _, _ = get_input_from_batch(src_seq)
 
-            mask_src = enc_batch.data.eq(config.PAD_idx).unsqueeze(1)
+        #mask_src = enc_batch.data.eq(config.PAD_idx).unsqueeze(1)
+        mask_src = tf.expand_dims(tf.math.equal(enc_batch, config.PAD_idx), axis = 1)
 
-            emb_mask = self.model.embedding(src_seq["mask_input"])
-            src_enc = self.model.encoder(self.model.embedding(enc_batch)+emb_mask,mask_src)
+        emb_mask = self.model.embedding(src_seq["mask_input"])
+        src_enc = self.model.encoder(self.model.embedding(enc_batch)+emb_mask,mask_src)
 
-            encoder_db = None
+        encoder_db = None
 
-            mask_transformer_db = None
-            DB_ext_vocab_batch = None
+        mask_transformer_db = None
+        DB_ext_vocab_batch = None
 
-            #-- Repeat data for beam search
-            n_bm = self.beam_size
-            n_inst, len_s, d_h = src_enc.size()
-            src_seq = enc_batch.repeat(1, n_bm).view(n_inst * n_bm, len_s)
-            src_enc = src_enc.repeat(1, n_bm, 1).view(n_inst * n_bm, len_s, d_h)
+        #-- Repeat data for beam search
+        n_bm = self.beam_size
+        #n_inst, len_s, d_h = src_enc.size()
+        n_inst, len_s, d_h = src_enc.shape
 
-            #-- Prepare beams
-            inst_dec_beams = [Beam(n_bm, device=self.device) for _ in range(n_inst)]
+        #src_seq = enc_batch.repeat(1, n_bm).view(n_inst * n_bm, len_s)
+        src_seq = tf.reshape(tf.tile(enc_batch, [1, n_bm]), [n_inst * n_bm, len_s])
+        #src_enc = src_enc.repeat(1, n_bm, 1).view(n_inst * n_bm, len_s, d_h)
+        src_enc = tf.reshape(tf.tile(src_enc, [1, n_bm, 1]), [n_inst * n_bm, len_s, d_h])
 
-            #-- Bookkeeping for active or not
-            active_inst_idx_list = list(range(n_inst))
-            inst_idx_to_position_map = get_inst_idx_to_tensor_position_map(active_inst_idx_list)
+        #-- Prepare beams
+        inst_dec_beams = [Beam(n_bm, device=self.device) for _ in range(n_inst)]
+
+        #-- Bookkeeping for active or not
+        active_inst_idx_list = list(range(n_inst))
+        inst_idx_to_position_map = get_inst_idx_to_tensor_position_map(active_inst_idx_list)
 
             #-- Decode
-            for len_dec_seq in range(1, max_dec_step + 1):
+        for len_dec_seq in range(1, max_dec_step + 1):
 
-                active_inst_idx_list = beam_decode_step(inst_dec_beams, len_dec_seq, src_seq, src_enc, inst_idx_to_position_map, n_bm, enc_batch_extend_vocab, extra_zeros, mask_src, encoder_db, mask_transformer_db, DB_ext_vocab_batch)
+            active_inst_idx_list = beam_decode_step(inst_dec_beams, len_dec_seq, src_seq, src_enc, inst_idx_to_position_map, n_bm, enc_batch_extend_vocab, extra_zeros, mask_src, encoder_db, mask_transformer_db, DB_ext_vocab_batch)
 
-                if not active_inst_idx_list:
-                    break  # all instances have finished their path to <EOS>
+            if not active_inst_idx_list:
+                break  # all instances have finished their path to <EOS>
 
-                src_seq, encoder_db, src_enc, inst_idx_to_position_map = collate_active_info(src_seq, encoder_db, src_enc, inst_idx_to_position_map, active_inst_idx_list)
+            src_seq, encoder_db, src_enc, inst_idx_to_position_map = collate_active_info(src_seq, encoder_db, src_enc, inst_idx_to_position_map, active_inst_idx_list)
 
         batch_hyp, batch_scores = collect_hypothesis_and_scores(inst_dec_beams, 1)
 
@@ -256,15 +283,21 @@ class Translator(object):
 
 def sequence_mask(sequence_length, max_len=None):
     if max_len is None:
-        max_len = sequence_length.data.max()
-    batch_size = sequence_length.size(0)
-    seq_range = torch.arange(0, max_len).long()
-    seq_range_expand = seq_range.unsqueeze(0).expand(batch_size, max_len)
+        #max_len = sequence_length.data.max()
+        max_len = tf.math.reduce_max(sequence_length)
+    #batch_size = sequence_length.size(0)
+    batch_size = sequence_length.shape[0]
+    #seq_range = torch.arange(0, max_len).long()
+    seq_range = tf.cast(tf.range(0, max_len), dtype = tf.int64)
+    #seq_range_expand = seq_range.unsqueeze(0).expand(batch_size, max_len)
+    seq_range_expand = tf.tile(tf.expand_dims(seq_range, 0), [batch_size,1])
+    print(seq_range_expand)
     seq_range_expand = seq_range_expand
     if sequence_length.is_cuda:
         seq_range_expand = seq_range_expand.cuda()
-    seq_length_expand = (sequence_length.unsqueeze(1)
-                        .expand_as(seq_range_expand))
+    #seq_length_expand = (sequence_length.unsqueeze(1).expand_as(seq_range_expand))
+    seq_length_expand = tf.broadcast_to(tf.expand_dims(sequence_length, 1),.seq_range_expand)
+    
     return seq_range_expand < seq_length_expand
     
 def get_input_from_batch(batch):
@@ -273,7 +306,8 @@ def get_input_from_batch(batch):
     batch_size, max_enc_len = enc_batch.size()
     assert enc_lens.size(0) == batch_size
 
-    enc_padding_mask = sequence_mask(enc_lens, max_len=max_enc_len).float()
+    #enc_padding_mask = sequence_mask(enc_lens, max_len=max_enc_len).float()
+    enc_padding_mask = tf.cast(sequence_mask(enc_lens, max_len=max_enc_len), dtype = tf.float32)
 
     extra_zeros = None
     enc_batch_extend_vocab = None
@@ -282,13 +316,16 @@ def get_input_from_batch(batch):
         enc_batch_extend_vocab = batch["input_ext_vocab_batch"]
         # max_art_oovs is the max over all the article oov list in the batch
         if batch["max_art_oovs"] > 0:
-            extra_zeros = torch.zeros((batch_size, batch["max_art_oovs"]))
+            #extra_zeros = torch.zeros((batch_size, batch["max_art_oovs"]))
+            extra_zeros = tf.zeros([batch_size, batch["max_art_oovs"]])
 
-    c_t_1 = torch.zeros((batch_size, 2 * config.hidden_dim))
+    #c_t_1 = torch.zeros((batch_size, 2 * config.hidden_dim))
+    c_t_1 = tf.zeros([batch_size, 2 * config.hidden_dim])
 
     coverage = None
     if config.is_coverage:
-        coverage = torch.zeros(enc_batch.size())
+        #coverage = torch.zeros(enc_batch.size())
+        coverage = tf.zeros(enc_batch.size())
 
     if config.USE_CUDA:
         if enc_batch_extend_vocab is not None:
