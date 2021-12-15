152995ad94225eab1e9f7c2562f81b5a508cbf7b
Tianqing <tz2372@gmail.com>
2021-12-15 15:57:34 -0500

    upload the psuedocode; need test

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -25,7 +25,7 @@ from utils.data_reader import load_dataset
 
 tf.random.set_seed(1)
 
-class Dataset(tf.keras.utils.Sequence):
+# class Dataset():
     """Custom data.Dataset compatible with data.DataLoader."""
     def __init__(self, data, vocab, batch_size, shuffle=True):
         """Reads source and target sequences from txt files."""
@@ -83,52 +83,104 @@ class Dataset(tf.keras.utils.Sequence):
         program[emo_map[emotion]] = 1
         return program, emo_map[emotion]
 
-def collate_fn(data):
-    def merge(sequences):
-        lengths = [len(seq) for seq in sequences]
-        padded_seqs = tf.ones([len(sequences), max(lengths)], dtype=int64) ## padding index 1
-        for i, seq in enumerate(sequences):
-            end = lengths[i]
-            padded_seqs[i, :end] = seq[:end]
-        return padded_seqs, lengths 
+def process_fn(data):
+  """Returns one data pair (source and target)."""
+  emo_map = {
+        'surprised': 0, 'excited': 1, 'annoyed': 2, 'proud': 3, 'angry': 4, 'sad': 5, 'grateful': 6, 'lonely': 7,
+        'impressed': 8, 'afraid': 9, 'disgusted': 10, 'confident': 11, 'terrified': 12, 'hopeful': 13, 'anxious': 14, 'disappointed': 15,
+        'joyful': 16, 'prepared': 17, 'guilty': 18, 'furious': 19, 'nostalgic': 20, 'jealous': 21, 'anticipating': 22, 'embarrassed': 23,
+        'content': 24, 'devastated': 25, 'sentimental': 26, 'caring': 27, 'trusting': 28, 'ashamed': 29, 'apprehensive': 30, 'faithful': 31}
+  item = {}
+  item["context_text"] = data["context"]
+  item["target_text"] = data["target"]
+  item["emotion_text"] = data["emotion"]
+
+  item["context"], item["context_mask"] = preprocess(item["context_text"])
+
+  item["target"] = preprocess(item["target_text"], anw=True)
+  item["emotion"], item["emotion_label"] = preprocess_emo(item["emotion_text"], emo_map)
+
+  return item
+
+def preprocess(vocab, arr, anw=False):
+  """Converts words to ids."""
+  if(anw):
+      sequence = [vocab.word2index[word] if word in vocab.word2index else config.UNK_idx for word in arr] + [config.EOS_idx]
+      return tf.convert_to_tensor(sequence)
+  else:
+      X_dial = [config.CLS_idx]
+      X_mask = [config.CLS_idx]
+      for i, sentence in enumerate(arr):
+          X_dial += [vocab.word2index[word] if word in vocab.word2index else config.UNK_idx for word in sentence]
+          spk = vocab.word2index["USR"] if i % 2 == 0 else vocab.word2index["SYS"]
+          X_mask += [spk for _ in range(len(sentence))] 
+      assert len(X_dial) == len(X_mask)
+
+      return tf.convert_to_tensor(X_dial), tf.convert_to_tensor(X_mask)
+
+def preprocess_emo(emotion, emo_map):
+    program = [0]*len(emo_map)
+    program[emo_map[emotion]] = 1
+
+    return program, emo_map[emotion]
 
 
-    data.sort(key=lambda x: len(x["context"]), reverse=True) ## sort by source seq
-    item_info = {}
-    for key in data[0].keys():
-        item_info[key] = [d[key] for d in data]
 
-    ## input
-    input_batch, input_lengths     = merge(item_info['context'])
-    mask_input, mask_input_lengths = merge(item_info['context_mask'])
+## to get the dataset
+import pandas as pd
+# records needs to be linked to the original dataset
+dataset = tf.data.Dataset.from_tensor_slices(pd.DataFrame.from_dict(records).to_dict(orient="list")).map(process_fn)
 
-    ## Target
-    target_batch, target_lengths   = merge(item_info['target'])
+# {'a': [0, 1], 'b': [2, 3]} -> [{'a': 0, 'b': [2,3]} -> [{"a": 0, 'b': 2}, {'a':1, 'b':3}]
+# ref: https://stackoverflow.com/questions/68567630/converting-a-list-of-dictionaries-to-a-tf-dataset
+
+######################
+# def collate_fn(data):
+#     def merge(sequences):
+#         lengths = [len(seq) for seq in sequences]
+#         padded_seqs = tf.ones([len(sequences), max(lengths)], dtype=tf.int64) ## padding index 1
+#         for i, seq in enumerate(sequences):
+#             end = lengths[i]
+#             padded_seqs[i, :end] = seq[:end]
+#         return padded_seqs, lengths 
 
 
-    if config.USE_CUDA:
-        input_batch = input_batch.cuda()
-        mask_input = mask_input.cuda()
-        target_batch = target_batch.cuda()
+#     data.sort(key=lambda x: len(x["context"]), reverse=True) ## sort by source seq
+#     item_info = {}
+#     for key in data[0].keys():
+#         item_info[key] = [d[key] for d in data]
+
+#     ## input
+#     input_batch, input_lengths     = merge(item_info['context'])
+#     mask_input, mask_input_lengths = merge(item_info['context_mask'])
+
+#     ## Target
+#     target_batch, target_lengths   = merge(item_info['target'])
+
+
+#     if config.USE_CUDA:
+#         input_batch = input_batch.cuda()
+#         mask_input = mask_input.cuda()
+#         target_batch = target_batch.cuda()
  
-    d = {}
-    d["input_batch"] = input_batch
-    d["input_lengths"] = tf.Variable(input_lengths, dtype=int64)
-    d["mask_input"] = mask_input
-    d["target_batch"] = target_batch
-    d["target_lengths"] = tf.Variable(target_lengths, dtype=int64)
-    ##program
-    d["target_program"] = item_info['emotion']
-    d["program_label"] = item_info['emotion_label']
+#     d = {}
+#     d["input_batch"] = input_batch
+#     d["input_lengths"] = tf.Variable(input_lengths, dtype=int64)
+#     d["mask_input"] = mask_input
+#     d["target_batch"] = target_batch
+#     d["target_lengths"] = tf.Variable(target_lengths, dtype=int64)
+#     ##program
+#     d["target_program"] = item_info['emotion']
+#     d["program_label"] = item_info['emotion_label']
 
-    ##text
-    d["input_txt"] = item_info['context_text']
-    d["target_txt"] = item_info['target_text']
-    d["program_txt"] = item_info['emotion_text']
-    return d 
+#     ##text
+#     d["input_txt"] = item_info['context_text']
+#     d["target_txt"] = item_info['target_text']
+#     d["program_txt"] = item_info['emotion_text']
+#     return d 
 
 
-def prepare_data_seq(batch_size=32):  
+# def prepare_data_seq(batch_size=32):  
 
     pairs_tra, pairs_val, pairs_tst, vocab = load_dataset()
 
